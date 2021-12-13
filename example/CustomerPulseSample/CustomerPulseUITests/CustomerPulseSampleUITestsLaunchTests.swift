eb31a6157082d5c9e466420323183904624ee761
emilien <emilienroussel9@gmail.com>
2021-12-13 10:00:44 +0100

    rename to customer pulse

---
+++ example/CustomerPulseSample/CustomerPulseUITests/CustomerPulseSampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CustomerPulse_sampleUITestsLaunchTests.swift
+//  CustomerPulse.sampleUITests
+//
+//  Created by emilien on 10/11/2021.
+//
+
+import XCTest
+
+class CustomerPulseSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ sdk/hm.ios.sdk/CustomerPulse/CustomerPulse.swift
@@ -0,0 +1,45 @@
+//
+//  CustomerPulse.swift
+//  CustomerPulse
+//
+//  Created by emilien on 10/11/2021.
+//
+
+import Foundation
+import UIKit
+
+public protocol CustomerPulseDelegate: AnyObject {
+    /// Automatically called whenever the user has successfully completed the survey
+    func csUserCompletedSurvey()
+}
+
+public class CustomerPulse {
+    let token: String
+    let baseURL: String = "https://survey.customerpulse.gov.ae"
+    
+    public weak var delegate: CustomerPulseDelegate?
+    
+    public init(_ token: String) {
+        self.token = token
+    }
+    
+    /// Displays the survey in a WKWebView.
+    ///
+    /// - Parameters:
+    ///   - vc: The view controller where the survey will pop up.
+    ///   - isDimissible: Defines whether the survey is dismissible by the user. Default is set at true.
+    ///   - dismissTimer: The value in milliseconds after which the questionnaire will close automatically after having been successfully completed. Default value is 1000ms.
+    ///   - options: Dictionary to specify optional parameters to load. (eg. lang='ar/en')
+    public func showSurvey(on vc: UIViewController, isDismissible: Bool = true, dimissAfter dismissTimer: Int = 1000, withOptions options:[String: Any] = [:]) -> Void {
+        let webView = CSWebView.init(surveyURL: "\(baseURL)/\(token)", isDismissible: isDismissible, dismissTimer: dismissTimer, withOptions: options) {
+            guard let sdkDelegate = self.delegate else {
+                return
+            }
+            
+            sdkDelegate.csUserCompletedSurvey()
+        }
+        
+        vc.isModalInPresentation = true
+        vc.present(webView, animated: true, completion: nil)
+    }
+}
